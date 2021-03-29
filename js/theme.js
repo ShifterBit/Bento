e5c2ec0d75839a707bdda21e0fb0a6e3bc545c27
ShifterBit <48891590+ShifterBit@users.noreply.github.com>
2021-03-29 11:50:19 +0000

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -36,12 +36,12 @@ themeToggle.addEventListener("click", () => {
 
 // Theme accordint the hour
 
-enableDark();
 const today = new Date();
-/*
+const hour = today.getHours();
+
 if (hour >= 19 || hour < 5) {
     enableDark();
 } else {
     disableDark();
 }
-*/
+
